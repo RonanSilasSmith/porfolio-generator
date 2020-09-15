91bf7cd5ddf8b933a02a139653f7c8b13505fa6e
RonanSilasSmith <smith.ronan@outlook.com>
2020-09-15 10:21:56 -0800

    pulls from master

---
+++ app.js
@@ -0,0 +1,12 @@
+const profileDataArgs = process.argv.slice(2,process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    console.log('=================');
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+    for (let i = 0; i < profileDataArr.length; i += 1){
+        console.log(profileDataArr[i]);
+    }
+}
+
+printProfileData(profileDataArgs);
